10e958ec57f5406639436c4366fab33754bcf2d3
spacemuggle <dimensional.inventions@gmail.com>
2020-06-12 17:11:28 -0400

    forced desktop env to handle automount

--- mount_usb.py
+++
@@ -1,9 +0,0 @@
-import os, subprocess
-
-# create mount directory
-mount_loc = "/media/pi/USB"
-subprocess.run(["sudo","mkdir","-p",mount_loc])
-
-# mount USB drive
-usb_loc = "/dev/sda1"
-subprocess.run(["sudo","mount",usb_loc,mount_loc])
